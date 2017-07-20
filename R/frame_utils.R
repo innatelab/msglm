6d71c269be3b7803deba29a1ab75581ff0e09c80
Alexey Stukalov <astukalov@gmail.com>
2017-07-20 22:32:19 +0000

    [*] move routines from fit_utils to frame_utils

--- R/frame_utils.R
+++ R/frame_utils.R
@@ -1,7 +1,64 @@
-# Utilities for pcp fitting
+# Utilities for assembling Stan report frames
 #
 # Author: Alexey Stukalov
 ###############################################################################
+
+process_model_fit_chunk <- function(file_ix, strip_samples=FALSE, strip_stats=FALSE) {
+  fit_model.file <- fit_model.files.df[file_ix, 'filename']
+  message('Loading ', fit_model.file, '...')
+  tmp.env <- new.env(parent = baseenv())
+  load(file.path(fit_model_path, fit_model.file), envir = tmp.env)
+  if (strip_samples || strip_stats) {
+    tmp.env$vars_results <- lapply( tmp.env$vars_results, function( var_results ) {
+      if (strip_samples) { var_results$samples <- NULL }
+      if (strip_stats) { var_results$stats <- NULL }
+      return ( var_results )
+    } )
+  }
+  gc()
+  res <- mget(ls(envir=tmp.env), envir=tmp.env)
+  return ( res )
+}
+
+process_msglm_chunk <- function(file_ix, sel_contrasts = NULL, condition_shifts = NULL,
+                                strip_samples=FALSE, strip_stats=FALSE,
+                                val_trans = NULL, condition.reported = "lhs",
+                                condition.quantiles_lhs = c(0, 1), condition.quantiles_rhs = c(0, 1),
+                                condition_agg_col = "condition") {
+  fit_model.file <- fit_model.files.df[ file_ix, 'filename' ]
+  message( 'Loading ', fit_model.file, '...' )
+  tmp.env <- new.env(parent = baseenv())
+  load(file.path(fit_model_path, fit_model.file ), envir = tmp.env)
+  if (!is.null(sel_contrasts)) {
+    # FIXME do once per assembly
+    sel_contrastXmetacondition <- contrastXmetacondition[sel_contrasts, , drop=FALSE]
+    sel_contrastXmetacondition <- sel_contrastXmetacondition[, colSums(abs(sel_contrastXmetacondition)) != 0, drop=FALSE]
+    sel_conditionXmetacondition.df <- dplyr::filter(conditionXmetacondition.df,
+                                                    metacondition %in% colnames(sel_contrastXmetacondition))
+    if (!is.null(condition_shifts)) {
+      sel_conditionXmetacondition.df <- dplyr::inner_join(sel_conditionXmetacondition.df, condition_shifts)
+    }
+    sel_contrastXcondition.df <- dplyr::semi_join(contrastXcondition.df, sel_conditionXmetacondition.df) %>%
+        dplyr::filter(contrast %in% sel_contrasts)
+    tmp.env$vars_results <- calc_contrasts(tmp.env$vars_results, tmp.env$vars_info, tmp.env$dims_info,
+                                           sel_contrastXmetacondition, sel_conditionXmetacondition.df,
+                                           sel_contrastXcondition.df, val_trans = val_trans,
+                                           condition_agg_col = condition_agg_col,
+                                           condition.reported = condition.reported,
+                                           condition.quantiles_lhs = condition.quantiles_lhs,
+                                           condition.quantiles_rhs = condition.quantiles_rhs)
+  }
+  if (strip_samples || strip_stats) {
+    tmp.env$vars_results <- lapply( tmp.env$vars_results, function( var_results ) {
+      if (strip_samples) { var_results$samples <- NULL }
+      if (strip_stats) { var_results$stats <- NULL }
+      return ( var_results )
+    } )
+  }
+  gc()
+  res <- mget(ls(envir=tmp.env), envir=tmp.env)
+  return ( res )
+}
 
 join_report_frames <- function(reports, frame_extractor=function(report) stop('no frame extractor'),
                                global_vars = c('model_dataset', 'version', 'chunk'),
@@ -30,6 +87,25 @@ join_report_frames <- function(reports, frame_extractor=function(report) stop('n
     joined_res.df <- dplyr::inner_join( joined_res.df, prot_info[,c('protein_ac_noiso','protein_label','description')] )
   }
   return ( joined_res.df )
+}
+
+join_msglm_reports <- function(section, reports, type) {
+  message( 'Assembling joint ', type, ' report for ', section, '...' )
+  res <- join_report_frames(reports, frame_extractor = function(report) {
+    res <- report$vars_results[[section]][[type]]
+    if (!is.null(res) && !('majority_protein_acs' %in% colnames(res))
+        && "majority_protein_acs" %in% colnames(report$model_data$protgroups)) {
+      res$majority_protein_acs <- rep_len(report$model_data$protgroups$majority_protein_acs[1], nrow(res))
+      res$protgroup_id <- rep_len(report$model_data$protgroups$protgroup_id[1], nrow(res))
+    }
+    if (!is.null(res) && !('pepmod_id' %in% colnames(res))
+        && "pepmod_id" %in% colnames(report$model_data$pepmods)) {
+      res$pepmod_id <- rep_len(report$model_data$pepmods$pepmod_id[1], nrow(res))
+    }
+    res
+  },
+  NULL, global_vars = c())
+  return(res)
 }
 
 rbind_all_frames <- function(frames_coll_list, frame_names = NULL, link_col = NULL)

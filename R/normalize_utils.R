7dc50e325e414192930dfed0d339f03147aefe8a
Alexey Stukalov <astukalov@gmail.com>
2017-10-08 14:07:25 +0000

    [*] norm_exps(): output msg when vars defined

--- R/normalize_utils.R
+++ R/normalize_utils.R
@@ -93,10 +93,6 @@ normalize_experiments <- function(stan_norm_model, stan_input_base, msdata_df,
         condgrp_msdata_df <- dplyr::semi_join(condgrp_msdata_df, sel_condgrp_objs)
         stan_input <- stan_input_base
         stan_input$Nobjects <- nrow(sel_condgrp_objs)
-        message("Normalizing group '", sel_condgrp_objs$cond_group[1], "', ",
-                stan_input$Nobjects, " object(s), ",
-                stan_input$Nconditions, " condition(s), ",
-                stan_input$Nmschannels, " mschannel(s)")
         if (nrow(condgrp_msdata_df) == 0L) {
             # degenerated case
             warning("No valid observations in group ", .$cond_group[1])
@@ -128,6 +124,10 @@ normalize_experiments <- function(stan_norm_model, stan_input_base, msdata_df,
         }
         stan_input$Nmschannels <- nrow(mschan_df)
         stan_input$Nconditions <- n_distinct(mschan_df$condition)
+        message("Normalizing group '", sel_condgrp_objs$cond_group[1], "', ",
+                stan_input$Nobjects, " object(s), ",
+                stan_input$Nconditions, " condition(s), ",
+                stan_input$Nmschannels, " mschannel(s)")
         stan_input$mschannel2condition <- as.array(as.integer(mschan_df$condition))
         stan_input$mschannel_shift <- as.array(mschan_df$shift)
         condgrp_msdata_df <- dplyr::mutate(condgrp_msdata_df,

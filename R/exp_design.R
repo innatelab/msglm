ae4b572c27e7a0ec0cda2109bda07d6844bb4c7b
Alexey Stukalov <astukalov@gmail.com>
2018-03-05 16:45:51 +0000

    [*] effect_factor(): enhance level extraction

--- R/exp_design.R
+++ R/exp_design.R
@@ -52,7 +52,8 @@ replicate_effects_matrix <- function(mschannels_df, msrun_col="msrun", cond_col=
 
 # extracts the value of given factor from the effect label
 effect_factor <- function(effects, factor_name, factor_levels, default = factor_levels[1]) {
-    factor_vals <- str_match(effects, paste0("(^|:)", factor_name, "([^:]+)($|:)"))[,3]
+    # FIXME factor levels are not allowed to start with '_' (to workaround factor with names being prefixes of the other factors)
+    factor_vals <- str_match(effects, paste0("(?:^|:)", factor_name, "([^:_][^:]*)(?:$|:)"))[,2]
     factor_vals[is.na(factor_vals)] <- default
     factor(factor_vals, levels=factor_levels)
 }

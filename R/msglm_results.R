14520eca5b76a767b243522f8c23b51e272a938e
Alexey Stukalov <astukalov@gmail.com>
2018-07-10 19:13:08 +0200

    [*] msglm.prepare_dims_info(): protregroup_id

--- R/msglm_results.R
+++ R/msglm_results.R
@@ -25,7 +25,8 @@ msglm.prepare_dims_info <- function(model_data, object_cols = NULL)
         dplyr::inner_join(objs_df)
   )
   if ("subobjects" %in% names(model_data)) {
-    res$subobject <- dplyr::select(model_data$subobjects, glm_object_ix, glm_subobject_ix, superprotgroup_id, pepmod_id)
+    res$subobject <- dplyr::select(model_data$subobjects, glm_object_ix, glm_subobject_ix,
+                                   one_of(c("protregroup_id", "protgroup_id")), pepmod_id)
   }
   if ("msproto_ix" %in% colnames(model_data$mschannels)) {
     res$msprotocol <- dplyr::select(model_data$mschannels, msproto_ix, one_of("instrument")) %>%

60873ce67ccd4610b8dfe8c2d4b5c7506792220d
Alexey Stukalov <astukalov@gmail.com>
2019-06-05 17:13:40 +0200

    [*] @importFrom: fix typo

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #' @useDynLib msglm
 #' @importFrom dplyr %>% tibble as_tibble mutate mutate_at select select_at
-#' @importForm dplyr summarise summarise_at rename rename_at arrange arrange_at group_by group_by_at
+#' @importFrom dplyr summarise summarise_at rename rename_at arrange arrange_at group_by group_by_at
 #' @importFrom dplyr row_number coalesce case_when if_else n n_distinct one_of cume_dist
 #' @importFrom stringr str_count str_detect str_subset str_match str_replace str_remove str_split_fixed
 NULL

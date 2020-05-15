0d8d097750e1feb3cefcb54a22733cd74abaf851
Alexey Stukalov <astukalov@gmail.com>
2020-05-15 14:36:16 +0200

    [.] any_of() is exported by tidyselect

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 #' @useDynLib msglm
 #' @importFrom dplyr %>% tibble as_tibble mutate mutate_at select select_at
 #' @importFrom dplyr summarise summarise_at rename rename_at arrange arrange_at group_by group_by_at
-#' @importFrom dplyr row_number coalesce case_when if_else n n_distinct any_of cume_dist
+#' @importFrom dplyr row_number coalesce case_when if_else n n_distinct cume_dist
+#' @importFrom tidyselect any_of
 #' @importFrom tidyr replace_na
 #' @importFrom stringr str_count str_detect str_subset str_match str_replace str_remove str_split_fixed
 #' @importFrom rlang %||%

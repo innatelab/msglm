41fbedd60e51384eaa2a6baa04f14d82023a94df
Alexey Stukalov <astukalov@gmail.com>
2021-08-13 22:22:20 +0200

    [*] ImportedValues: std::vector -> boost::array

--- src/imported_values.cpp
+++ src/imported_values.cpp
@@ -2,6 +2,7 @@
 
 #include "logging.h"
 
+#include <boost/array.hpp>
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics/stats.hpp>
 #include <boost/accumulators/statistics/extended_p_square.hpp>
@@ -14,12 +15,9 @@ void ImportedValues::init_bounds(double support_prob)
          "support_prob=" << support_prob << ", should be in [0.5, 1.0] range" );
     double valmin = !values.empty() ? values[0] : std::numeric_limits<value_t>::quiet_NaN();
     double valmax = !values.empty() ? values[0] : std::numeric_limits<value_t>::quiet_NaN();
-    const double outlier_prob = 0.5*(1.0 - support_prob);
-    std::vector<double> quant_probs(2); // FIXME pre C++11 legacy, use initializer list
-    quant_probs[0] = outlier_prob;
-    quant_probs[1] = 1.0 - outlier_prob;
+    const double tail_prob = 0.5*(1.0 - support_prob);
     bacc::accumulator_set<double, bacc::stats<bacc::tag::extended_p_square> >
-        quant_acc(bacc::tag::extended_p_square::probabilities = quant_probs);
+        quant_acc(bacc::tag::extended_p_square::probabilities = boost::array<double, 2>{tail_prob, 1.0 - tail_prob});
     if (!values.empty()) quant_acc(values[0]);
     for ( std::size_t i = 1; i < values.size(); ++i ) {
       const double val = values[i];
@@ -31,6 +29,6 @@ void ImportedValues::init_bounds(double support_prob)
     val_max = valmax;
     support_min = bacc::extended_p_square(quant_acc)[0];
     support_max = bacc::extended_p_square(quant_acc)[1];
-    LOG_DEBUG2(    "val_min=" << val_min << " support_min=" << support_min
-                << " support_max=" << support_max << " val_max=" << val_max );
+    LOG_DEBUG2(    "minmax=[" << val_min << ", " << val_max << "] "
+                << "support=[" << support_min << ", " << support_max << "]" );
 }

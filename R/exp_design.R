e88eae6562d7d271090c5fab0da624806238f2fc
Alexey Stukalov <astukalov@gmail.com>
2021-09-08 11:16:16 +0200

    [-] replace_effects_matrix(): unused
    the generation is implemented directly in the Stan model

--- R/exp_design.R
+++ R/exp_design.R
@@ -15,43 +15,6 @@ conditionXeffect_frame <- function(conditionXeffect_mtx, effects_df) {
     dplyr::ungroup()
 }
 
-# compose replicate effects design matrix
-#' @export
-replicate_effects_matrix <- function(mschannels_df, msrun_col="msrun", condition_col="condition", replicate_col="msrun",
-                                     replicate_contrast = "contr.poly") {
-  # compose replicate effects
-  # ~ (replicate %in% condition) doesn't work because of assymetrical design (diff. N of replicates)
-  mschannels_df <- dplyr::select_at(mschannels_df, c("msrun"=msrun_col, "condition"=condition_col, "replicate"=replicate_col))
-  msrunXreplEffect_pre.df <- dplyr::group_by(mschannels_df, condition) %>%
-    dplyr::do({
-      if (n_distinct(.$replicate) <= 1) {
-        # return stub to keep msrun
-        warning(.$condition[[1]], ": single replicate (", .$msrun[[1]], "), no replicate effects")
-        tibble(msrun = as.character(.$msrun),
-               repl_effect = paste0("__ignore_replicate__", .$condition[[1]]),
-               w = 0)
-      } else {
-        cond_msruns.df <- dplyr::mutate(., replicate=factor(replicate))
-        mtx <- model.matrix(~ replicate,
-                          cond_msruns.df,
-                          contrasts.arg = list("replicate" = replicate_contrast))
-        mtx <- mtx[, colnames(mtx) != "(Intercept)", drop=FALSE]
-        dimnames(mtx) <- list(msrun = cond_msruns.df$msrun,
-                              repl_effect = paste0(condition_col, cond_msruns.df$condition[1], ':', colnames(mtx)))
-        as_tibble(as.table(mtx)) %>% dplyr::filter(n != 0.0) %>%
-          dplyr::rename(w = n) %>%
-          dplyr::mutate(msrun = as.character(msrun),
-                        repl_effect = as.character(repl_effect))
-      }
-    }) %>% dplyr::ungroup()
-  repl_col <- paste0(replicate_col, "_effect")
-  res <- frame2matrix(msrunXreplEffect_pre.df %>%
-                      dplyr::rename_at("repl_effect", function(x) replicate_col) %>%
-                      dplyr::rename_at("msrun", function(x) msrun_col),
-                      msrun_col, replicate_col, "w")
-  res[, !str_detect(colnames(res), "__ignore_replicate__"), drop=FALSE]
-}
-
 # extracts the value of given factor from the effect label
 #' @export
 effect_factor <- function(effects, factor_name, factor_levels, default = factor_levels[1]) {

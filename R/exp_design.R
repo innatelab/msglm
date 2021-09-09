cc5a9bd24484abd78897a62982adcf7b999fb4f9
Alexey Stukalov <astukalov@gmail.com>
2021-09-09 15:10:38 +0200

    [-] exp_design.R: conditionXeffect_frame() unused

--- R/exp_design.R
+++ R/exp_design.R
@@ -1,20 +1,3 @@
-# convert conditionXeffect matrix to a frame and calculate
-# condition2effect weights
-#' @export
-conditionXeffect_frame <- function(conditionXeffect_mtx, effects_df) {
-  effects_df <- maybe_rename(effects_df, c("prior_tau" = "tau"))
-  tibble::as_tibble(as.table(conditionXeffect_mtx)) %>%
-    dplyr::filter(n != 0) %>% dplyr::rename(mult = n) %>%
-    # estimate the weight of a condition in an effect
-    dplyr::inner_join(dplyr::select(effects_df, effect, prior_tau)) %>%
-    dplyr::mutate(w = prior_tau * abs(mult)) %>%
-    dplyr::group_by_at(names(dimnames(conditionXeffect_mtx))[[1]]) %>%
-    dplyr::mutate(eff_w = w/sum(w)) %>%
-    dplyr::group_by_at(names(dimnames(conditionXeffect_mtx))[[2]]) %>%
-    dplyr::mutate(cond_w = eff_w/sum(eff_w)) %>%
-    dplyr::ungroup()
-}
-
 # extracts the value of given factor from the effect label
 #' @export
 effect_factor <- function(effects, factor_name, factor_levels, default = factor_levels[1]) {
